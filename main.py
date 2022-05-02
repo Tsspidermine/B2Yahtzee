c669924cf1b08669621c4eb02813573124e702c2
tsspidermine <51246489+Tsspidermine@users.noreply.github.com>
2022-05-01 23:34:47 -0600

    Added more scoring methods

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import tkinter as tk
 import random
-import playsound
+# import playsound TODO: Add sound
 from scoring_functions import *
 
 window = tk.Tk()
@@ -19,64 +19,72 @@ dice_rolls = []
 # Create the canvas list
 canvas_list = []
 for i in range(5):
-  roll = random.randint(1,6)
-  dice_rolls.append(roll)
-  canvas_list.append(tk.Canvas(
-    window,
-    width=100,
-    height=100,
-  ))
-  canvas_list[i].grid(row=0,column = i)
-  canvas_list[i].create_image(
-      0,
-      0,
-      anchor=tk.NW,
-      image = dice_images[dice_rolls[i] - 1]
-  )
+    roll = random.randint(1,6)
+    dice_rolls.append(roll)
+    canvas_list.append(tk.Canvas(
+        window,
+        width=100,
+        height=100,
+    ))
+    canvas_list[i].grid(row=0,column = i)
+    canvas_list[i].create_image(
+        0,
+        0,
+        anchor=tk.NW,
+        image = dice_images[dice_rolls[i] - 1]
+    )
 
 def roll_dice():
-  for i in range(5):
-    if enable[i].get() == 0:
-      roll = random.randint(0,5)
-      # update the dice_rolls
-      dice_rolls[i] = roll + 1
-      canvas_list[i].create_image(
-          0,
-          0,
-          anchor=tk.NW,
-          image = dice_images[roll]
-      )
-  score_helper()
+    for i in range(5):
+        if enable[i].get() == 0:
+            roll = random.randint(0,5)
+            # update the dice_rolls
+            dice_rolls[i] = roll + 1
+            canvas_list[i].create_image(
+                0,
+                0,
+                anchor=tk.NW,
+            image = dice_images[roll]
+        )
+    score_helper()
 
 # Score helper function
+yahtzee_count = 0
 def score_helper():
-  lbl_chance['text'] = "Chance: "+str(chance(dice_rolls))
-  for i in range(6):
-    lbl_numbers[i]['text'] = str(i+1) + "'s: " + str(singles(dice_rolls,i+1))
+    lbl_chance['text'] = "Chance: "+str(chance(dice_rolls))
+    lbl_threes['text'] = "Three of a kind: "+str(three_of_kind(dice_rolls))
+    lbl_fours['text'] = "Four of a kind: "+str(four_of_kind(dice_rolls))
+    lbl_fulls['text'] = "Full house: "+str(full_house(dice_rolls))
+    lbl_sm['text'] = "Small straight: "+str(sm_straight(dice_rolls))
+    lbl_lg['text'] = "Large straight: "+str(lg_straight(dice_rolls))
+    sm_straight(dice_rolls)
+    # Update the singles lables
+    for i,lbl in enumerate(lbl_singles):
+      lbl['text'] = str(i+1) + "'s: " + str(singles(dice_rolls,i+1))
 
 chkBox_list = []
 
 enable = {
-    0:tk.IntVar(),
-    1:tk.IntVar(),
-    2:tk.IntVar(),
-    3:tk.IntVar(),
-    4:tk.IntVar(),
+     0:tk.IntVar(),
+     1:tk.IntVar(),
+     2:tk.IntVar(),
+     3:tk.IntVar(),
+     4:tk.IntVar(),
 }
 
 for i in range(5):
-  # create and append new checkbox
-  chkBox_list.append(tk.Checkbutton(
-    window,
-    text = "Keep",
-    variable = enable[i],
-    onvalue=1,
-    offvalue=0,
-    ))
-  for i in range(len(chkBox_list)):
-    chkBox_list[i].grid(
-        row = 1,
-        column = i)
+    # create and append new checkbox
+    chkBox_list.append(tk.Checkbutton(
+      window,
+      text = "Keep",
+      variable = enable[i],
+      onvalue=1,
+      offvalue=0,
+      ))
+    for i in range(len(chkBox_list)):
+      chkBox_list[i].grid(
+          row = 1,
+          column = i)
 
 btn_roll_dice = tk.Button(text="Roll the Dice!",command=roll_dice)
 btn_roll_dice.grid(row=2, column=2)
@@ -84,10 +92,25 @@ btn_roll_dice.grid(row=2, column=2)
 lbl_chance = tk.Label(text="Chance: ")
 lbl_chance.grid(row=2, column=4)
 
-# Create the labels for 1's, 2's, 3's, etc.
-lbl_numbers = []
+lbl_threes = tk.Label(text="Three of a kind: ")
+lbl_threes.grid(row=2, column=5)
+
+lbl_fours = tk.Label(text="Four of a kind: ")
+lbl_fours.grid(row=3, column=5)
+
+lbl_fulls = tk.Label(text="Full house: ")
+lbl_fulls.grid(row=4, column=5)
+
+lbl_sm = tk.Label(text="Small straight: ")
+lbl_sm.grid(row=5, column=5)
+
+lbl_lg = tk.Label(text="Small straight: ")
+lbl_lg.grid(row=6, column=5)
+
+# Create the labels for singles
+lbl_singles = [tk.Label(text= str(i+1) + "'s: ") for i in range(6)]
 for i in range(6):
-  lbl_numbers.append(tk.Label(text= str(i+1) + "'s: "))
-  lbl_numbers[i].grid(row=3+i, column=4)
+    lbl_singles[i].grid(row=3+i, column=4)
+
 
 tk.mainloop()
--- scoring_functions.py
+++ scoring_functions.py
@@ -1,24 +1,77 @@
 # This file has Yahtzee scoring function
 def chance(dice_rolls):
-  """Given a list of dice rolls, return the sum of the dice"""
-  return sum(dice_rolls)
+    """Given a list of dice rolls, return the sum of the dice"""
+    return sum(dice_rolls)
 
 def singles(dice_rolls,number):
-  # declare a sum
-  sum = 0
-  # loop over every dice roll
-  for roll in dice_rolls:
-    # if the roll is a one, add 1 to the sum, if it's a two, add 2, etc.
-    if roll == number:
-      sum += number
-  return sum
+    # declare a sum
+    sum = 0
+    # loop over every dice roll
+    for roll in dice_rolls:
+        # if the roll is a one, add 1 to the sum, if it's a two, add 2, etc.
+        if roll == number:
+            sum += number
+    return sum
 
 def three_of_kind(dice_rolls):
-  # loop over each number 1 through 6
-  for i in range(1,7):
-    # if the ount of number in dice_rolls is >= 3
-    if dice_rolls.count(i) >= 3:
-      # return chance(dice_rolls)
-      return chance(dice_rolls)
-  return 0
+    # loop over each number 1 through 6
+    for i in range(1,7):
+        # if the count of number in dice_rolls is >= 3
+        if dice_rolls.count(i) >= 3:
+            # return chance(dice_rolls)
+            return chance(dice_rolls)
+    return 0
+
+def four_of_kind(dice_rolls):
+    # loop over each number 1 through 6
+    for i in range(1,7):
+        # if the count of number in dice_rolls is >= 3
+        if dice_rolls.count(i) >= 4:
+            # return chance(dice_rolls)
+            return chance(dice_rolls)
+    return 0
+
+# TODO: Add a 100 point linear bonus for every yahtzee
+def yahtzee(dice_rolls):
+    for i in range(1,7):
+        # if the count of the number in dice_rolls is >= 5
+        if dice_rolls.count(i) >= 5:
+            return 50 #+ (100 * yahtzee_count)
+    return 0
+
+def full_house(dice_rolls):
+    three_house = False
+    two_house = False
+    for i in range(1,7):
+        if dice_rolls.count(i) == 3:
+            three_house = True
+        if dice_rolls.count(i) == 2:
+            two_house = True
+        if three_house and two_house:
+            return 25
+    return 0
+
+# TODO: I forgot to make sure it is a list from 1-6 (1,2,3,4,5,6), but forgot to account for
+# cases like a full house, where it's (2,2,2,5,5)
+def sm_straight(dice_rolls):
+    sm_rolls = []
+    sm_rolls.extend(dice_rolls)
+    sm_rolls.pop()
+    if sorted(sm_rolls) == sm_rolls:
+        return 30
+    return 0
+
+# TODO: Refer to line 54
+def lg_straight(dice_rolls):
+    if sorted(dice_rolls) == dice_rolls:
+        return 40
+    return 0
+
+
+
+
+
+
+
+
 
